cfc265f777b5f85457f2da50628a0f52c433a14f
jplegacy <jeremyperez1122@gmail.com>
2023-06-23 16:01:25 -0400

    AuthCode Authorization Request Partial Test

--- internal/grants/authorization_code_test.go
+++ internal/grants/authorization_code_test.go
@@ -3,20 +3,24 @@ package grants
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 	"time"
 
 	"github.com/scorpio-id/oauth/internal/config"
 	"github.com/scorpio-id/oauth/pkg/oauth"
+	"github.com/stretchr/testify/assert"
 )
 
 // TestAuthorizationCodeGrant produces JWT via authorization code grant flow (link to rfc)
 func TestAuthorizationCodeGrant(t *testing.T) {
 	// TODO - test authorization code grant flow ...
-	cfg := config.NewConfig("internal/config/test.yml")
+	cfg := config.NewConfig("../config/test.yml")
 
 	// generate an RSA key pair
 	private, err := rsa.GenerateKey(rand.Reader, cfg.OAuth.RSABits)
@@ -35,12 +39,78 @@ func TestAuthorizationCodeGrant(t *testing.T) {
 	granter := NewGranter(issuer, minutes, 8, name+"/device")
 
 	server := httptest.NewServer(http.HandlerFunc(granter.AuthorizationCodeHandler))
-	server.Start()
 	defer server.Close()
 
 	// TODO - implement client portion of authorization code grant!
+
+	client := http.Client{
+		// Client Auto Follows Redirects but we want the redirecturi
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}}
+
 	// ex step 1: GET http://localhost:8081/authorize?client_id=friday&response_type=code&redirect_uri=https://client.example.io
-	// DO NOT follow HTTP 302, simply read response to get authorization code
+
+	client_id := "friday"
+	redirect_url := "https://client.example.io"
+
+	authCodeEndpoint := fmt.Sprintf("%v/authorize?client_id=%v&response_type=code&redirect_uri=%v", server.URL, client_id, redirect_url)
+
+	getReq, err := http.NewRequest("GET", authCodeEndpoint, nil)
+	if err != nil{
+		log.Printf("%v", err)
+	}
+
+	getReq.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := client.Do(getReq)
+	if  err != nil{
+		log.Printf("%v", err)
+	}
+
+	// Makes sure response is a redirect
+	assert.True(t, resp.StatusCode == 302)
+
+	redirectUri, err := resp.Location()
+	if  err != nil{
+		log.Printf("%v", err)
+	}
+
+	// Extracting url code parameter
+	u, err := url.Parse(redirectUri.String())
+	if  err != nil{
+		log.Printf("%v", err)
+	}
+
+	q := u.Query()
+	authCode := q["code"][0] // Grabs the first code given back
+
 	// ex step 2: POST http://localhost:8081/jwt?grant_type=authorization_code&client_id=friday&code=7deced1b-edac-439d-b990-c6cee1df8fd2&redirect_uri=https://client.example.io
-	// remember HTTP headers ... Content-Type application/x-www-form-urlencoded
+
+	// ------ AUTHORIZATION ------
+
+	jwtEndpoint := fmt.Sprintf("%v/jwt?grant_type=authorization_code&client_id=%v&code=%v&response_type=code&redirect_uri=%v", server.URL, client_id, authCode, redirect_url)
+
+	postReq, err := http.NewRequest("POST", jwtEndpoint, nil)
+	if err != nil{
+		log.Printf("%v", err)
+	}
+
+	postReq.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err = client.Do(postReq)
+	if  err != nil{
+		log.Printf("%v", err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if  err != nil{
+		log.Printf("%v", err)
+	}
+	defer resp.Body.Close()
+
+	// GOING TO REDIRECT URI FOR SOME REASON
+
+	fmt.Printf("%v",body)
+
 }
