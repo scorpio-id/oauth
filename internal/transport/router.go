6095ec303e54b484006c1b37167b7992c8df9139
brandon <b.mazey@nyu.edu>
2025-07-09 13:11:43 -0400

    added registration API to router

--- internal/transport/router.go
+++ internal/transport/router.go
@@ -53,6 +53,9 @@ func NewRouter(cfg config.Config) (*mux.Router, *grants.Granter) {
 	// host oauth2 JWKS endpoint
 	router.HandleFunc(cfg.OAuth.JWKS, issuer.JWKSHandler)
 
+	// host registration endpoint
+	router.HandleFunc("/register", granter.RegistrationHandler).Methods(http.MethodPost, http.MethodOptions)
+
 	// host grant endpoints
 	router.HandleFunc("/token", granter.ClientCredentialsHandler).Methods(http.MethodPost, http.MethodOptions)
 	router.HandleFunc("/authorize", granter.AuthorizationCodeHandler).Methods(http.MethodGet, http.MethodOptions)
